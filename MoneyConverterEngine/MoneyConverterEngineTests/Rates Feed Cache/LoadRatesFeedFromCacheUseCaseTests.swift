d31d42287f6cbf7b5a9e94117e25bed3fbb6a5ba
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2020-09-08 15:43:50 +0100

    test: local loader init does not send messages to the local store upon creation

---
+++ MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/LoadRatesFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,26 @@
+//
+// Copyright © 2020 Stormwright. All rights reserved.
+//
+
+import XCTest
+import MoneyConverterEngine
+
+class LoadRatesFeedFromCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalRatesFeedLoader, store: RatesFeedStoreSpy) {
+        let store = RatesFeedStoreSpy()
+        let sut = LocalRatesFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+}
