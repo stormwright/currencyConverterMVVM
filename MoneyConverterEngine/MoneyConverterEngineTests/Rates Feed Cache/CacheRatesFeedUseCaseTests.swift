72ffe4c96b4743c33e72060c3011be4a7274bdde
Stormwright <mmikayil@gmail.com>
2020-09-08 14:45:51 +0100

    test: move RatesFeedStoreSpy to the designated file

--- MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/CacheRatesFeedUseCaseTests.swift
+++ MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/CacheRatesFeedUseCaseTests.swift
@@ -177,52 +177,6 @@ protocol RatesFeedStore {
     func retrieve(completion: @escaping RetrieveCompletion)
 }
 
-class RatesFeedStoreSpy: RatesFeedStore {
-    
-    enum ReceivedMessage: Equatable {
-        case deleteCachedFeed
-        case insert([LocalFeedRate], Date)
-        case retrieve
-    }
-    
-    private(set) var receivedMessages = [ReceivedMessage]()
-    
-    private var deleteCompletions = [DeleteCompletion]()
-    private var insertCompletions = [InsertCompletion]()
-    private var retrievalCompletions = [RetrieveCompletion]()
-    
-    func deleteCachedFeed(completion: @escaping DeleteCompletion) {
-        deleteCompletions.append(completion)
-        receivedMessages.append(.deleteCachedFeed)
-    }
-    
-    func completeDelete(with error: Error, at index: Int = 0) {
-        deleteCompletions[index](.failure(error))
-    }
-    
-    func completeDeleteSuccessfully(at index: Int = 0) {
-        deleteCompletions[index](.success(()))
-    }
-    
-    func insert(_ feed: [LocalFeedRate], timestamp: Date, completion: @escaping InsertCompletion) {
-        insertCompletions.append(completion)
-        receivedMessages.append(.insert(feed, timestamp))
-    }
-    
-    func completeInsert(with error: Error, at index: Int = 0) {
-        insertCompletions[index](.failure(error))
-    }
-    
-    func completeInsertSuccessfully(at index: Int = 0) {
-        insertCompletions[index](.success(()))
-    }
-    
-    func retrieve(completion: @escaping RetrieveCompletion) {
-        
-    }
-    
-
-}
 
 class LocalRatesFeedLoader: RatesFeedLoader {
     
---
+++ MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/Helpers/RatesFeedStoreSpy.swift
@@ -0,0 +1,56 @@
+//
+//  RatesFeedStoreSpy.swift
+//  MoneyConverterEngineTests
+//
+//  Copyright © 2020 Stormwright. All rights reserved.
+//
+
+import Foundation
+import MoneyConverterEngine
+
+class RatesFeedStoreSpy: RatesFeedStore {
+    
+    enum ReceivedMessage: Equatable {
+        case deleteCachedFeed
+        case insert([LocalFeedRate], Date)
+        case retrieve
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deleteCompletions = [DeleteCompletion]()
+    private var insertCompletions = [InsertCompletion]()
+    private var retrievalCompletions = [RetrieveCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeleteCompletion) {
+        deleteCompletions.append(completion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func completeDelete(with error: Error, at index: Int = 0) {
+        deleteCompletions[index](.failure(error))
+    }
+    
+    func completeDeleteSuccessfully(at index: Int = 0) {
+        deleteCompletions[index](.success(()))
+    }
+    
+    func insert(_ feed: [LocalFeedRate], timestamp: Date, completion: @escaping InsertCompletion) {
+        insertCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    
+    func completeInsert(with error: Error, at index: Int = 0) {
+        insertCompletions[index](.failure(error))
+    }
+    
+    func completeInsertSuccessfully(at index: Int = 0) {
+        insertCompletions[index](.success(()))
+    }
+    
+    func retrieve(completion: @escaping RetrieveCompletion) {
+        
+    }
+    
+
+}
