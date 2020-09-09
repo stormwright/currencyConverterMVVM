9cb7b46ab77b15601ba2cbdfd337d47e506ab070
Stormwright <mmikayil@gmail.com>
2020-09-09 11:50:05 +0100

    test: upon validate cache request local loader does not delete non expired cache

--- MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -31,6 +31,18 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_validateCache_doesNotDeleteNonExpiredCache() {
+        let feed = uniqueRatesFeed()
+        let fixedCurrentDate = Date()
+        let nonExpiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.validateCache { _ in }
+        store.completeRetrieve(with: feed.local, timestamp: nonExpiredTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalRatesFeedLoader, store: RatesFeedStoreSpy) {
