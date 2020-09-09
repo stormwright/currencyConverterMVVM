33838cce76a053d5bd994fc85df687819357c48e
Stormwright <mmikayil@gmail.com>
2020-09-09 10:15:31 +0100

    test: local loader has no side effects on retrieve error

--- MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/LoadRatesFeedFromCacheUseCaseTests.swift
+++ MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/LoadRatesFeedFromCacheUseCaseTests.swift
@@ -71,6 +71,15 @@ class LoadRatesFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_load_hasNoSideEffectsOnRetrieveError() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        store.completeRetrieve(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalRatesFeedLoader, store: RatesFeedStoreSpy) {
