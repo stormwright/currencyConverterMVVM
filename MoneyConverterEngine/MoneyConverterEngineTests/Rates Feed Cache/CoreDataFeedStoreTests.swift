3ec28275f6ff6574ec7a22c33e597fef78122285
Stormwright <mmikayil@gmail.com>
2020-09-10 12:08:58 +0100

    test: on retrieve request the local data store has no side effects on empty cache

--- MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/CoreDataFeedStoreTests.swift
+++ MoneyConverterEngine/MoneyConverterEngineTests/Rates Feed Cache/CoreDataFeedStoreTests.swift
@@ -14,7 +14,9 @@ class CoreDataFeedStoreTests: XCTestCase, RatesFeedStoreSpecs {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
